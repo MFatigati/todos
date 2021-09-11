04e5713dc612f2a3258f2af8769e24ce648cab3c
MFatigati <fatigati.michael@gmail.com>
2021-09-11 08:21:23 -0400

    Deployment

--- lib/users.sql
+++ lib/users.sql
@@ -1,4 +1,4 @@
 INSERT INTO users (username, password)
-  VALUES ('admin', 'secret'),
-         ('developer', 'letmein'),
+  VALUES ('admin', '$2b$10$uC3lSZqheRZja2B.jBA8q.2s3hkEviwTuRWdw36tngJBkm1i/Llo6'),
+         ('developer', '$2b$10$RIwuQnP.NKc1gJ5Ef08.dOSX8ibuEnDMziqJGr925nRM0LRhvCFN.'),
          ('somebody', 'knock-knock');
